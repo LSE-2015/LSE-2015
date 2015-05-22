bafb2a7787653548ccaa657f1e158920401117a4
Pablo Cholbi Alenda <p.cholbi@alumnos.upm.es>
2015-05-22 15:43:33 +0200

    [camctrl] Update

--- UART-Controller/src/main.c
+++ UART-Controller/src/main.c
@@ -4,32 +4,28 @@
 #include <math.h>
 #include "STM32vldiscovery.h"
 
- /* BUTTON GPIOs define ------------------------------------------------------------*/
+ /* BUTTON GPIOs define ------------------------------------------------------*/
 #define BUTTON_LEFT         GPIO_Pin_0
 #define BUTTON_RIGHT        GPIO_Pin_1
-#define BUTTON_UP         	GPIO_Pin_2
+#define BUTTON_UP             GPIO_Pin_2
 #define BUTTON_DOWN         GPIO_Pin_3
 
 #define BUTTON_SCREEN_PORT     GPIOA
 #define BUTTON_DRONE_PORT      GPIOC
 #define BUTTONPORTCLK   RCC_APB2Periph_GPIOA
 
-
-
-
- /* Private macro -------------------------------------------------------------*/
- /* Private variables ---------------------------------------------------------*/
+ /* Private macro ------------------------------------------------------------*/
+ /* Private variables --------------------------------------------------------*/
  TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
  TIM_OCInitTypeDef  TIM_OCInitStructure;
 
- /* Private function prototypes -----------------------------------------------*/
+ /* Private function prototypes ----------------------------------------------*/
  void RCC_Configuration(void);
  void GPIO_Configuration(void);
  void EnableTimerInterrupt(void);
  void USART1_Init(void);
 
- /* Private functions ---------------------------------------------------------*/
-
+ /* Private functions --------------------------------------------------------*/
 
  int main(void)
  {
@@ -52,8 +48,7 @@
 
    EnableTimerInterrupt();
 
-   while (1)
-   {}
+   while(1) {}
  }
 
  /**
@@ -69,20 +64,19 @@
    /* GPIOA and GPIOB clock enable */
    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |
                           RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO, ENABLE);
-
  }
 
  void GPIO_Configuration(void)
  {
    GPIO_InitTypeDef GPIO_InitStructure;
 
-   /* GPIOA Configuration:TIM3 Channel1, 2, 3 and 4 as alternate function push-pull */
-   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+   /* GPIOA Configuration: TIM3 Channel 1, 2, 3 and 4 */
+   /* as alternate function push-pull */
+   GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_6 | GPIO_Pin_7;
+   GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF_PP;
    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
    GPIO_Init(GPIOA, &GPIO_InitStructure);
-
 
    //using same structure as with TIMER3, we will initialize button pin
 
@@ -133,7 +127,6 @@
    //select input pull-up
    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
    GPIO_Init(BUTTON_DRONE_PORT, &GPIO_InitStructure);
-
  }
 
  /*****************************************************
@@ -152,14 +145,14 @@ void USART1_Init(void)
                            RCC_APB2Periph_GPIOA, ENABLE);
 
     /* GPIOA PIN9 alternative function Tx */
-    gpioa_init_struct.GPIO_Pin = GPIO_Pin_9;
+    gpioa_init_struct.GPIO_Pin   = GPIO_Pin_9;
     gpioa_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
-    gpioa_init_struct.GPIO_Mode = GPIO_Mode_AF_PP;
+    gpioa_init_struct.GPIO_Mode  = GPIO_Mode_AF_PP;
     GPIO_Init(GPIOA, &gpioa_init_struct);
     /* GPIOA PIN9 alternative function Rx */
-    gpioa_init_struct.GPIO_Pin = GPIO_Pin_10;
+    gpioa_init_struct.GPIO_Pin   = GPIO_Pin_10;
     gpioa_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
-    gpioa_init_struct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    gpioa_init_struct.GPIO_Mode  = GPIO_Mode_IN_FLOATING;
     GPIO_Init(GPIOA, &gpioa_init_struct);
 
     /* Enable USART1 */
@@ -167,12 +160,13 @@ void USART1_Init(void)
     /* Baud rate 9600, 8-bit data, One stop bit
      * No parity, Do both Rx and Tx, No HW flow control
      */
-    usart1_init_struct.USART_BaudRate = 9600;
+    usart1_init_struct.USART_BaudRate   = 9600;
     usart1_init_struct.USART_WordLength = USART_WordLength_8b;
-    usart1_init_struct.USART_StopBits = USART_StopBits_1;
-    usart1_init_struct.USART_Parity = USART_Parity_No ;
-    usart1_init_struct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-    usart1_init_struct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    usart1_init_struct.USART_StopBits   = USART_StopBits_1;
+    usart1_init_struct.USART_Parity     = USART_Parity_No ;
+    usart1_init_struct.USART_Mode       = USART_Mode_Rx | USART_Mode_Tx;
+    usart1_init_struct.USART_HardwareFlowControl =
+                                                 USART_HardwareFlowControl_None;
     /* Configure USART1 */
     USART_Init(USART1, &usart1_init_struct);
     /* Enable RXNE interrupt */
@@ -185,39 +179,46 @@ void USART1_Init(void)
  void EnableTimerInterrupt()
   {
       NVIC_InitTypeDef nvicStructure;
-      nvicStructure.NVIC_IRQChannel = TIM3_IRQn;
+      nvicStructure.NVIC_IRQChannel                   = TIM3_IRQn;
       nvicStructure.NVIC_IRQChannelPreemptionPriority = 0;
-      nvicStructure.NVIC_IRQChannelSubPriority = 1;
-      nvicStructure.NVIC_IRQChannelCmd = ENABLE;
+      nvicStructure.NVIC_IRQChannelSubPriority        = 1;
+      nvicStructure.NVIC_IRQChannelCmd                = ENABLE;
       NVIC_Init(&nvicStructure);
   }
 
-//Se ejecuta cada 20 ms
+ // Se ejecuta cada 20 ms
  void TIM3_IRQHandler()
  {
-	 int command_send;
+     int command_send;
      if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
      {
          TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
 
-         //int left,right,up,down,red_button;
-         int left_scr,right_scr,up_scr,down_scr,left_dron,right_dron,up_dron,down_dron;
-         //read JOYSTICK every 20 ms (when UPDATE event of TIM3 is generated)
+         int left_scr,right_scr,up_scr,down_scr;
+         int left_dron,right_dron,up_dron,down_dron;
 
-         left_scr=GPIO_ReadInputDataBit(BUTTON_SCREEN_PORT, BUTTON_LEFT);
-         right_scr=GPIO_ReadInputDataBit(BUTTON_SCREEN_PORT, BUTTON_RIGHT);
-         up_scr=GPIO_ReadInputDataBit(BUTTON_SCREEN_PORT, BUTTON_UP);
-         down_scr=GPIO_ReadInputDataBit(BUTTON_SCREEN_PORT, BUTTON_DOWN);
+         // read JOYSTICK every 20 ms (when UPDATE event of TIM3 is generated)
 
-		 left_dron=GPIO_ReadInputDataBit(BUTTON_DRONE_PORT, BUTTON_LEFT);
-         right_dron=GPIO_ReadInputDataBit(BUTTON_DRONE_PORT, BUTTON_RIGHT);
-         up_dron=GPIO_ReadInputDataBit(BUTTON_DRONE_PORT, BUTTON_UP);
-         down_dron=GPIO_ReadInputDataBit(BUTTON_DRONE_PORT, BUTTON_DOWN);
+         left_scr   = GPIO_ReadInputDataBit(BUTTON_SCREEN_PORT, BUTTON_LEFT);
+         right_scr  = GPIO_ReadInputDataBit(BUTTON_SCREEN_PORT, BUTTON_RIGHT);
+         up_scr     = GPIO_ReadInputDataBit(BUTTON_SCREEN_PORT, BUTTON_UP);
+         down_scr   = GPIO_ReadInputDataBit(BUTTON_SCREEN_PORT, BUTTON_DOWN);
+
+         left_dron  = GPIO_ReadInputDataBit(BUTTON_DRONE_PORT, BUTTON_LEFT);
+         right_dron = GPIO_ReadInputDataBit(BUTTON_DRONE_PORT, BUTTON_RIGHT);
+         up_dron    = GPIO_ReadInputDataBit(BUTTON_DRONE_PORT, BUTTON_UP);
+         down_dron  = GPIO_ReadInputDataBit(BUTTON_DRONE_PORT, BUTTON_DOWN);
 
 
-         command_send = (left_dron * pow (2,7))+(right_dron * pow (2,6))+(up_dron * pow (2,5))+(down_dron * pow (2,4))+(left_scr * pow (2,3))+(right_scr * pow (2,2))+(up_scr * pow (2,1))+(down_scr * pow (2,0));
+         command_send = (left_dron  * pow (2,7))+
+                        (right_dron * pow (2,6))+
+                        (up_dron    * pow (2,5))+
+                        (down_dron  * pow (2,4))+
+                        (left_scr   * pow (2,3))+
+                        (right_scr  * pow (2,2))+
+                        (up_scr     * pow (2,1))+
+                        (down_scr   * pow (2,0));
 
-		 USART_SendData(USART1, command_send);
-
+         USART_SendData(USART1, command_send);
      }
  }
